6cf3826d032ccc1a2cde3fea78c9447d987e8927
Shohanur <shohan.drmc@gmail.com>
2021-03-27 19:51:15 +0600

    initial-commit

---
+++ src/main/java/com/example/logingregistration/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.example.logingregistration.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/com/example/logingregistration/LogingRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logingregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogingRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
